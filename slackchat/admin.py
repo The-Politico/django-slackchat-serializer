0f7561567ce3b4b9967e98c01ae85dbdc4cbfe72
Tyler Fisher <tfisher@politico.com>
2017-12-21 12:14:39 -0500

    add some new admins

--- slackchat/admin.py
+++ slackchat/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from .models import Channel, Message, Reaction, User
+from .models import Channel, ChatType, Message, Reaction, User
 
 
 class ChannelAdmin(admin.ModelAdmin):
     readonly_fields = ('api_id', 'name',)
-    fields = ('api_id', 'name', 'owner')
+    fields = ('api_id', 'name', 'owner', 'chat_type')
 
 
 admin.site.register(Channel, ChannelAdmin)
 admin.site.register(User)
 admin.site.register(Message)
 admin.site.register(Reaction)
+admin.site.register(ChatType)
