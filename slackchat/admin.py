8925c6a1db60f77fb96df16d07de4962b8451424
Jon McClure <jmcclure@politico.com>
2018-04-11 14:00:06 -0400

    admin actions

--- slackchat/admin.py
+++ slackchat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .celery import post_webhook, post_webhook_republish, update_users
 from .models import (Argument, Attachment, Channel, ChatType,
                      CustomContentTemplate, KeywordArgument, Message, Reaction,
                      User, Webhook)
@@ -29,10 +30,39 @@ class ChannelAdmin(admin.ModelAdmin):
         'live',
     )
     autocomplete_fields = ['owner']
+    actions = [
+        'request_chats_update',
+        'request_chats_republish',
+        'close_live_chats',
+    ]
+
+    def request_chats_update(self, request, queryset):
+        for channel in queryset:
+            post_webhook.delay(channel.id.hex, channel.chat_type.name)
+        self.message_user(request, 'Requested channels update chat page!')
+
+    def request_chats_republish(self, request, queryset):
+        for channel in queryset:
+            post_webhook_republish.delay(
+                channel.id.hex, channel.chat_type.name)
+        self.message_user(request, 'Requested channels republish chat page!')
+
+    def close_live_chats(self, request, queryset):
+        queryset.update(live=False)
+        for channel in queryset:
+            post_webhook.delay(channel.id.hex, channel.chat_type.name)
+        self.message_user(
+            request,
+            'Closed live channels and requested they update chat page!')
 
 
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
+    actions = ['update_user_profiles']
+
+    def update_user_profiles(self, request, queryset):
+        update_users.delay([user.pk for user in queryset])
+        self.message_user(request, 'Updated user profiles!')
 
 
 class WebhookAdmin(admin.ModelAdmin):
