0c14e0779d80582de3422d52c5487538e774d212
Jon McClure <jmcclure@politico.com>
2018-10-31 07:53:45 -0400

    use prefetch_related for channel messages. roll as 0.5.4

--- slackchat/viewsets.py
+++ slackchat/viewsets.py
@@ -8,20 +8,20 @@ from .serializers import (ChannelListSerializer, ChannelSerializer,
 class ChannelViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
-    lookup_field = 'pk'
+    lookup_field = "pk"
     authentication_classes = []
     permission_classes = []
     pagination_class = None
     throttle_classes = []
 
     def get_serializer_class(self):
-        if hasattr(self, 'action') and self.action == 'list':
+        if hasattr(self, "action") and self.action == "list":
             return ChannelListSerializer
         return ChannelSerializer
 
     def get_queryset(self):
-        queryset = Channel.objects.all()
-        chat_type = self.request.query_params.get('chat_type', None)
+        queryset = Channel.objects.prefetch_related("messages__reactions")
+        chat_type = self.request.query_params.get("chat_type", None)
         if chat_type:
             queryset = queryset.filter(chat_type__name=chat_type)
         return queryset
