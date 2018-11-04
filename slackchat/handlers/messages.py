aded2e48fe55ffebc32b2dea5e7a9b62cf89d7b6
Andrew Briz <briz.andrew@gmail.com>
2018-11-04 15:42:58 -0500

    0.5.4: Handle bug where deleting messages with threads don't delete.

--- slackchat/handlers/messages.py
+++ slackchat/handlers/messages.py
@@ -9,11 +9,7 @@ from slackchat.models import Channel, KeywordArgument, Message, User
 
 from .attachments import handle as handle_attachment
 
-ignored_subtypes = [
-    'group_join',
-    'file_share',
-    'group_archive',
-]
+ignored_subtypes = ["group_join", "file_share", "group_archive"]
 
 
 def strptimestamp(timestamp):
@@ -22,46 +18,44 @@ def strptimestamp(timestamp):
 
 def handle_removed(id, event):
     try:
-        channel = Channel.objects.get(
-            api_id=event.get('channel')
-        )
+        channel = Channel.objects.get(api_id=event.get("channel"))
     except ObjectDoesNotExist:
         return
     msg = {
-        'user': event.get('previous_message').get('user'),
-        'ts': event.get('previous_message').get('ts'),
-        'text': event.get('message', {}).get('text', None)
+        "user": event.get("previous_message").get("user"),
+        "ts": event.get("previous_message").get("ts"),
+        "text": event.get("message", {}).get("text", None),
     }
-    user, created = User.objects.get_or_create(
-        api_id=msg.get('user')
-    )
+    user, created = User.objects.get_or_create(api_id=msg.get("user"))
 
     # If thread
-    if event.get('previous_message', {}).get('thread_ts'):
+    if not event.get("message", {}).get(
+        "subtype"
+    ) == "tombstone" and event.get("previous_message", {}).get("thread_ts"):
         if not channel.chat_type.kwargs_in_threads:
             return
 
-        thread = event.get('previous_message')
+        thread = event.get("previous_message")
 
         try:
             original_message = Message.objects.get(
-                timestamp=strptimestamp(thread.get('thread_ts'))
+                timestamp=strptimestamp(thread.get("thread_ts"))
             )
         except ObjectDoesNotExist:
             raise MessageNotFoundError(
-                '{}'.format(strptimestamp(thread.get('thread_ts')))
+                "{}".format(strptimestamp(thread.get("thread_ts")))
             )
 
         try:
-            user = User.objects.get(api_id=thread.get('user'))
+            user = User.objects.get(api_id=thread.get("user"))
         except ObjectDoesNotExist:
-            raise UserNotFoundError(thread.get('user'))
+            raise UserNotFoundError(thread.get("user"))
 
         try:
             kwarg = KeywordArgument.objects.get(
-                timestamp=strptimestamp(thread.get('ts')),
+                timestamp=strptimestamp(thread.get("ts")),
                 message=original_message,
-                user=user
+                user=user,
             )
         except ObjectDoesNotExist:
             return
@@ -70,9 +64,7 @@ def handle_removed(id, event):
 
     else:
         Message.objects.get(
-            channel=channel,
-            timestamp=strptimestamp(msg.get('ts')),
-            user=user,
+            channel=channel, timestamp=strptimestamp(msg.get("ts")), user=user
         ).delete()
 
 
@@ -85,49 +77,46 @@ def handle(id, event):
     marker = MarkSlack(
         user_templates=user_templates,
         link_templates=settings.MARKSLACK_LINK_TEMPLATES,
-        image_template=settings.MARKSLACK_IMAGE_TEMPLATE
+        image_template=settings.MARKSLACK_IMAGE_TEMPLATE,
     )
 
     try:
-        channel = Channel.objects.get(
-            api_id=event.get('channel')
-        )
+        channel = Channel.objects.get(api_id=event.get("channel"))
     except ObjectDoesNotExist:
         return
 
-    subtype = event.get('subtype', None)
+    subtype = event.get("subtype", None)
     if subtype in ignored_subtypes:
         return
 
     if subtype:
         msg = {
-            'user': event.get('previous_message').get('user'),
-            'ts': event.get('previous_message').get('ts'),
-            'text': event.get('message', {}).get('text', None)
+            "user": event.get("previous_message").get("user"),
+            "ts": event.get("previous_message").get("ts"),
+            "text": event.get("message", {}).get("text", None),
         }
     else:
         msg = {
-            'user': event.get('user'),
-            'ts': event.get('ts'),
-            'text': event.get('text')
+            "user": event.get("user"),
+            "ts": event.get("ts"),
+            "text": event.get("text"),
         }
 
-    user, created = User.objects.get_or_create(
-        api_id=msg.get('user')
-    )
+    user, created = User.objects.get_or_create(api_id=msg.get("user"))
 
-    thread_ts = event.get('thread_ts', None) or \
-        event.get('message', {}).get('thread_ts', None)
+    thread_ts = event.get("thread_ts", None) or event.get("message", {}).get(
+        "thread_ts", None
+    )
     # If thread
     if thread_ts and (
-        event.get('parent_user_id', None) or
-        event.get('message', {}).get('parent_user_id', None)
+        event.get("parent_user_id", None)
+        or event.get("message", {}).get("parent_user_id", None)
     ):
         if not channel.chat_type.kwargs_in_threads:
             return
 
         try:
-            key, value = msg.get('text').split(': ', 1)
+            key, value = msg.get("text").split(": ", 1)
         except ValueError:
             # Don't handle threads that don't follow key-value pattern
             return
@@ -137,30 +126,23 @@ def handle(id, event):
                 timestamp=strptimestamp(thread_ts)
             )
         except ObjectDoesNotExist:
-            raise MessageNotFoundError(
-                '{}'.format(strptimestamp(thread_ts))
-            )
+            raise MessageNotFoundError("{}".format(strptimestamp(thread_ts)))
         KeywordArgument.objects.update_or_create(
-            timestamp=strptimestamp(msg.get('ts')),
+            timestamp=strptimestamp(msg.get("ts")),
             message=original_message,
             user=user,
-            defaults={
-                "key": key,
-                "value": value
-            }
+            defaults={"key": key, "value": value},
         )
     else:
         message, created = Message.objects.update_or_create(
             channel=channel,
-            timestamp=strptimestamp(msg.get('ts')),
+            timestamp=strptimestamp(msg.get("ts")),
             user=user,
-            defaults={
-                'text': marker.mark(msg.get('text'))
-            }
+            defaults={"text": marker.mark(msg.get("text"))},
         )
 
         # Attachments
-        attachments = event.get('message', {}).get('attachments')
+        attachments = event.get("message", {}).get("attachments")
         if attachments:
             for attachment in attachments:
                 handle_attachment(message.pk, attachment)
--- slackchat/views.py
+++ slackchat/views.py
@@ -6,8 +6,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from slackchat.conf import settings
 
-from .handlers import (handle_message, handle_message_removed,
-                       handle_reaction_added, handle_reaction_removed)
+from .handlers import (
+    handle_message,
+    handle_message_removed,
+    handle_reaction_added,
+    handle_reaction_removed,
+)
 
 
 class Events(APIView):
@@ -19,32 +23,38 @@ class Events(APIView):
         slack_message = request.data
 
         # import json
-        # print('DEBUG slackchat log:')
+        #
+        # print("DEBUG slackchat log:")
         # print(json.dumps(slack_message, indent=2))
 
-        if slack_message.get('token') != settings.SLACK_VERIFICATION_TOKEN:
+        if slack_message.get("token") != settings.SLACK_VERIFICATION_TOKEN:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
-        if slack_message.get('type') == 'url_verification':
+        if slack_message.get("type") == "url_verification":
             return Response(
-                data=slack_message.get('challenge'),
-                status=status.HTTP_200_OK
+                data=slack_message.get("challenge"), status=status.HTTP_200_OK
             )
 
-        if 'event' in slack_message:
-            id = slack_message.get('event_id')
-            event = slack_message.get('event')
+        if "event" in slack_message:
+            id = slack_message.get("event_id")
+            event = slack_message.get("event")
 
-            if event.get('type') == 'message':
-                if event.get('subtype', None) == 'message_deleted':
+            if event.get("type") == "message":
+                if event.get("subtype", None) == "message_deleted":
+                    handle_message_removed(id, event)
+                elif (
+                    event.get("subtype", None) == "message_changed"
+                    and event.get("message", {}).get("subtype", None)
+                    == "tombstone"
+                ):
                     handle_message_removed(id, event)
                 else:
                     handle_message(id, event)
 
-            if event.get('type') == 'reaction_added':
+            if event.get("type") == "reaction_added":
                 handle_reaction_added(id, event)
 
-            if event.get('type') == 'reaction_removed':
+            if event.get("type") == "reaction_removed":
                 handle_reaction_removed(id, event)
 
         return Response(status=status.HTTP_200_OK)
